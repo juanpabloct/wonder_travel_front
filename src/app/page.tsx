f36c865d5ec34681578f0e4d321c217d0468bb59
juanpabloct <juanptorres2003@gmail.com>
2023-07-04 01:21:45 -0500

    Add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,39 @@
 import { Main } from "@/components/main";
 import { Details } from "@/components/detalles";
-import viajero from "../../public/viajero.png";
-import viajero2 from "../../public/viajero (1).png";
-import wonderOrange from "../../public/Group 1101.svg";
-import tucan from "../../public/tucan.png";
-import puestaDeSol from "../../public/puesta-de-sol.png";
-import ballena from "../../public/ballena (2).png";
-import amazonas from "../../public/amazonas (3).png";
-import montañas from "../../public/montanas (1).png";
-import PersonAdventure from "../../public/noun_adventure_68979.svg";
+
 import Image from "next/image";
-import { PountsTravel } from "@/components/pountsTravel";
+import { DescriptionTravel } from "@/components/pountsTravel";
 import { Flex } from "@/components/tw-components/flex";
+import {
+  PersonAdventure,
+  acotur,
+  amazonas,
+  ballena,
+  footer,
+  montañas,
+  puestaDelSol,
+  tucan,
+  viajero1,
+  viajero2,
+  wonderBlack,
+  wonderOrange,
+} from "../../public/icons";
 
 export default function Home() {
   return (
     <main className=" min-h-screen">
-      <Flex
-        col
-        className="items-center justify-between  relative gap-6 bg-blackLight"
-      >
+      <header className=" flex flex-col items-center justify-between  relative gap-6 bg-blackLight">
         <Main />
         <Details />
-      </Flex>
-      <Flex col className="bg-beige text-black justify-center">
+      </header>
+      <Flex col className="bg-beige text-black ">
         <h3 className="text-xs text-center w-10/12 font-bold py-4 self-center">
           Wonder Travel es la plataforma que conecta viajeros con anfitriones en
           lugares mágicos de Colombia
         </h3>
         <Flex>
           <Flex>
-            <Image src={viajero} alt="Viajero1" />
+            <Image src={viajero1} alt="Viajero1" />
             <Image src={viajero2} alt="Viajero2" />
           </Flex>
           <Image src={wonderOrange} alt="Wonder Orange" />
@@ -38,7 +41,7 @@ export default function Home() {
             <Image src={tucan} alt="" width={28} />
             <Image src={amazonas} alt="" width={28} />
             <Image className="col-span-2" src={montañas} alt="" width={28} />
-            <Image src={puestaDeSol} alt="" width={28} />
+            <Image src={puestaDelSol} alt="" width={28} />
             <Image src={ballena} alt="" width={28} />
           </div>
         </Flex>
@@ -46,17 +49,35 @@ export default function Home() {
           <Image src={PersonAdventure} alt="Wonder Orange" />
         </Flex>
         <Flex className="">
-          <Flex col width="w-2/3">
+          <Flex col width="w-2/3" className="max-w-2xl">
             <Flex className="justify-between">
               <h3 className="text-xl font-semibold">La travesía</h3>
               <Flex col className="">
                 <span>Wonder Travel</span>
               </Flex>
             </Flex>
-            <PountsTravel />
+            <DescriptionTravel />
           </Flex>
         </Flex>
       </Flex>
+      <footer className="bg-beige text-black">
+        <Flex col className="items-center pb-16">
+          <div className="grid grid-cols-2 justify-items-center items-center gap-2 divide-x-2 divide-orange-500 mb-3">
+            <Image src={wonderBlack} alt="Logo en negro" />
+            <Flex className="h-1/2 items-center pl-6">
+              <h4 className="">#ViajaAlMomento</h4>
+            </Flex>
+          </div>
+          <Image src={footer} alt="footer" className="mb-3" />
+          <h2 className="underline my-2 ">Miembro de:</h2>
+          <Image src={acotur} alt="Acotur" />
+        </Flex>
+        <Flex className="bg-black p-2">
+          <p className="text-[#FDFCF7] text-[15px]">
+            © Copyright Wonder Travel
+          </p>
+        </Flex>
+      </footer>
     </main>
   );
 }
