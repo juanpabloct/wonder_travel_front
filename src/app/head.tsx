11b1eabb3e5189341b9a22fd93aa3be4543d0061
juanpabloct <juanptorres2003@gmail.com>
2023-07-03 22:46:45 -0500

    Import font Abril Fatface

---
+++ src/app/head.tsx
@@ -0,0 +1,14 @@
+const Head = () => {
+  return (
+    <>
+      <head>
+        <style>
+          {`
+          @import url('https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap');
+          /* Otros estilos CSS que desees aplicar */
+        `}
+        </style>
+      </head>
+    </>
+  );
+};
