f154db0f0004835a3a757335df9ba6ecc8997fe0
juanpabloct <juanptorres2003@gmail.com>
2023-07-03 22:46:59 -0500

    add custom styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,13 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      backgroundColor: {
+        "blackLight": "#1a1c1f",
+        "beige": "#FDFCF7"
+      },
+      fontFamily: {
+        abrilFatFace: ['Abril Fatface', 'cursive'],
+      },
     },
   },
   plugins: [],
