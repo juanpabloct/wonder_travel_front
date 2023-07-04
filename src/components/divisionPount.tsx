bfab72af684f5cc74096007509ef99713128c077
juanpabloct <juanptorres2003@gmail.com>
2023-07-04 01:22:32 -0500

    More border width

--- src/components/divisionPount.tsx
+++ src/components/divisionPount.tsx
@@ -1,7 +1,7 @@
 export const DivisionPount = () => {
   return (
     <div className="grid grid-cols-3 justify-items-center">
-      <div className="w-0 h-10 border-dashed col-span-1 col-start-2  border-orange-500 border-r-2 "></div>
+      <div className="w-0 h-10  border-dashed col-span-1 col-start-2  border-orange-500 border-r-4 "></div>
     </div>
   );
 };
