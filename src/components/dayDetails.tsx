c00cd7c0932a2e50ee299c6ff69e5c4ac2c8ac4f
juanpabloct <juanptorres2003@gmail.com>
2023-07-04 01:22:07 -0500

    Adjust component day

--- src/components/dayDetails.tsx
+++ src/components/dayDetails.tsx
@@ -1,14 +1,25 @@
 import { DivisionPount } from "./divisionPount";
 import { Flex } from "./tw-components/flex";
 
-export const DayDetails = ({ day }: { day: string }) => {
+export const DayDetails = ({
+  day,
+  startLine = true,
+}: {
+  day: string;
+  startLine?: boolean;
+}) => {
   return (
-    <div className="grid grid-cols-3">
+    <div className="grid grid-cols-3 ">
       <div className="col-start-2">
-        <DivisionPount />
+        {startLine && (
+          <>
+            <DivisionPount />
+            <DivisionPount />
+          </>
+        )}
       </div>
-      <Flex col className="h-full">
-        <h4 className="text-right border-t w-32 border-orange-500 font-bold text-orange-500 self-end mt-3">
+      <Flex col className="h-full row-span-2">
+        <h4 className="text-right border-t w-4/5 md:w-2/3 border-orange-500 font-bold text-orange-500 self-end mt-3 translate-y-4">
           {day}
         </h4>
       </Flex>
