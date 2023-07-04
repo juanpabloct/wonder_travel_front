203f5b7ae81a8c6958d08cb6e9008ca3b9969f0b
juanpabloct <juanptorres2003@gmail.com>
2023-07-04 01:23:02 -0500

    add style person

--- src/components/icon.tsx
+++ src/components/icon.tsx
@@ -1,9 +1,17 @@
 import Image from "next/image";
 import { Flex } from "./tw-components/flex";
 
-export const Icon = ({ icon }: { icon?: string }) => {
+export const Icon = ({
+  icon,
+  styleIcon,
+}: {
+  icon?: string;
+  styleIcon?: string;
+}) => {
   return (
-    <Flex className="h-8 w-8 col-start-2 bg-[#FF6549] rounded-full">
+    <Flex
+      className={`h-8 w-8 col-start-2 bg-[#FF6549] rounded-full ${styleIcon}`}
+    >
       {icon && <Image src={icon} alt="" width={20} height={20} />}
     </Flex>
   );
